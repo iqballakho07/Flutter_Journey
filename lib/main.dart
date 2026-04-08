ab2d02b476ff09035a654e0e32d5e9cde2b359c2
Muhammad Iqbal <muhammadiqballakho07@gmail.com>
2026-04-08 15:58:02 +0500

    day 6

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Container',
       theme: ThemeData(primaryColor: Colors.blue),
-      home: const MyHomePage(title: 'Flutter Container Example'),
+      home: const MyHomePage(title: 'Flutter '),
     );
   }
 }
@@ -34,6 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        centerTitle: true,
 
         title: Text(widget.title),
       ),
@@ -80,7 +81,111 @@ class _MyHomePageState extends State<MyHomePage> {
       //   },
       //   child: Text("click me"),
       // ),
-      body : Image.asset("assets/images/pic.jpg")
+      // body : Image.asset(r"assets/images/pic.jpg")
+      // body:Container(
+      //   width : 300,
+      // child : Row(
+      //   // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: [
+      //        Text("Mr."),
+      //        Text("Muhammad"),
+      //        Text("Iqbal")
+      //   ],
+      // )
+      // child: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     Text("M"),
+      //     Text("U"),
+      //     Text("E"),
+      //     Text("T")
+      //   ],
+      // ),
+
+      //small ui 
+      // body: Center(
+      //   child: Container(
+      //     height: 500,
+      //     width: 300,
+      //     decoration: BoxDecoration(
+      //       borderRadius: BorderRadius.circular(20),
+      //       border: Border.all(color: Colors.black, width: 2),
+      //     ),
+      //     child: Column(
+      //       children: [
+      //         Text(
+      //           "Profile",
+      //           style: TextStyle(
+      //             color: const Color.fromARGB(255, 124, 215, 238),
+      //             fontSize: 20,
+      //             fontWeight: FontWeight.bold,
+      //           ),
+      //         ),
+      //         ClipRRect(
+      //           borderRadius: BorderRadius.circular(20),
+      //           child: SizedBox(
+      //             height: 250,
+      //             width: 270,
+      //             child: Image.asset(
+      //               "assets/images/pic.jpg",
+      //               width: double.infinity,
+      //               fit: BoxFit.fill,
+      //             ),
+      //           ),
+      //         ),
+      //         Container(
+      //           // padding: EdgeInsets.all(20),
+      //           padding: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 10),
+                
+      //           child: Text("Hi, This is Muhammad Iqbal . I am undergradute student at Mehran university Jamshoro currently learning flutter ",
+      //               style: TextStyle(
+      //                 fontWeight: FontWeight.bold
+      //               ),),
+      //         ),
+      //         Container(
+      //           padding: EdgeInsets.only(left: 20,bottom: 10),
+      //           alignment: Alignment.centerLeft,
+      //           child: Text("Contact me : 0309-3196901", style: TextStyle(
+      //             fontWeight: FontWeight.bold
+      //            ),
+      //           ),
+      //         ),
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //           children: [
+      //             ElevatedButton(onPressed: (){}, child: Text("Facebook",style: TextStyle(fontWeight: FontWeight.bold),)),
+      //             ElevatedButton(onPressed: (){}, child: Text("Whatsapp",style: TextStyle(fontWeight: FontWeight.bold)))
+      //           ],
+      //         ),
+      //         Container(
+      //           padding: EdgeInsets.only(top: 15),
+      //           child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             Text("Rate us :      ",style: TextStyle(fontWeight: FontWeight.bold),),
+      //             Image.asset("assets/images/rating.png",width: 20,height: 20,fit:BoxFit.fill),
+      //             Image.asset("assets/images/rating.png",width: 20,height: 20,fit:BoxFit.fill),
+      //             Image.asset("assets/images/rating.png",width: 20,height: 20,fit:BoxFit.fill),
+      //             Image.asset("assets/images/rating.png",width: 20,height: 20,fit:BoxFit.fill),
+      //             Image.asset("assets/images/rating.png",width: 20,height: 20,fit:BoxFit.fill),
+      //           ],
+      //         )
+      //         )   
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body : InkWell(
+        onTap: () => print("Tapped"),
+        child : Container(
+        width: 200,
+        height: 200,
+        color: Colors.blue,
+      )
+      )
     );
   }
 }
